129ab4d9e475fdfb07d467e07fb6a07be711b4be
kasimotkun <kasimotkun@gmail.com>
2020-06-23 14:37:52 +0300

    Update run.bat

--- bin/Debug/netcoreapp3.1/run.bat
+++ bin/Debug/netcoreapp3.1/run.bat
@@ -1,2 +1,3 @@
-C:\temp\GfK_TestAutomationTask2\bin\Debug\netcoreapp3.1\GfK_TestAutomationTask2.exe methodName=GetEmployees threadCount=10
+C:\temp\SampleAPILoadTestProjectWithMultithreadedConsoleApp\bin\Debug\netcoreapp3.1\SampleAPILoadTestProjectWithMultithreadedConsoleApp.exe methodName=GetEmployees threadCount=10
+C:\temp\SampleAPILoadTestProjectWithMultithreadedConsoleApp\bin\Debug\netcoreapp3.1\SampleAPILoadTestProjectWithMultithreadedConsoleApp.exe methodName=UpdateEmployeeById threadCount=10
 PAUSE
